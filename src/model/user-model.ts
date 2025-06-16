bd5d1560e8451d3bf4414882aef9b7a8d19a7d66
= <=>
2025-06-16 15:58:44 -0300

    feat: implement UserModel class for user data management

---
+++ src/model/user-model.ts
@@ -0,0 +1,31 @@
+import { UserProps } from "../types/user-types"
+
+export class UserModel {
+    readonly id: string;
+    public name: string;
+    private email: string;
+    private password: string;
+    public created_at: Date;
+    public updated_at: Date;
+
+    constructor (props: UserProps) {
+        this.id = props.id;
+        this.name = props.name;
+        this.email = props.email;
+        this.password = props.password;
+        this.created_at = props.created_at || new Date();
+        this.updated_at = props.updated_at || new Date ();
+    }
+
+    getPassword () {
+        return this.password;
+    }
+
+    setPassword (newPassword: string) {
+        this.password = newPassword;
+    }
+
+    getEmail () {
+        return this.email;
+    }
+}
