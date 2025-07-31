c392ea01f27ac81bbe11f08b4ec2b12c32029fde
= <=>
2025-07-31 17:08:01 -0300

    feat: add transaction routes to the main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import userRoutes from './user-routes';
+import transactionRoutes from './transaction-routes';
 
 const router = Router();
 
 router.use('/users', userRoutes);
+router.use('/transactions', transactionRoutes);
 
 export default router;
