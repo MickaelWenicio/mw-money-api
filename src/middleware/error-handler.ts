2103f8299a34f8225e7b5796a95be7bc30e0a55b
= <=>
2025-08-19 13:48:01 -0300

    feat: add error middleware to avoid unnecessary code and improve error handling

---
+++ src/middleware/error-handler.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from '../utils/app-error';
+
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    console.error('Unhandled error:', err); 
+
+    if (err instanceof AppError) {
+        res.status(err.statusCode).json({ message: err.message });
+    }
+
+    res.status(500).json({ message: 'Internal server error' });
+};
