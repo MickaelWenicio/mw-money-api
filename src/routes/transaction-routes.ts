8d58333b02ff910778d6ab60a26a13e6c0005f47
= <=>
2025-08-22 15:58:40 -0300

    feat: add route to update transaction

--- src/routes/transaction-routes.ts
+++ src/routes/transaction-routes.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post('/', transactionController.create);
 router.get('/:userId', transactionController.getByUserId);
 router.delete('/:transactionId', transactionController.deleteById);
+router.put('/:transactionId', transactionController.updateById);
 
 export default router;
